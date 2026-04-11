8c4f7243150e1604fbbd5b24379c41cc4f54efb1
tachuka <75483726+detrit1@users.noreply.github.com>
2026-04-11 16:32:24 -0300

    Update testData.py
    senha2

--- testData.py
+++ testData.py
@@ -4,7 +4,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="BaconFrito2.0",
+    password="senhaBoa",
     database="producao_db"
 )
 
